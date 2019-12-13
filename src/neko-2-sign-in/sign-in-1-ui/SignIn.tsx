ac39b1d474d4a9a4871e962f69624a792e7f793f
ibulavsky <ibulavsky@gmail.com>
2019-12-13 20:33:47 +0300

    Added hooks (useState), and methods (onChange, onSubmit).

--- src/neko-2-sign-in/sign-in-1-ui/SignIn.tsx
+++ src/neko-2-sign-in/sign-in-1-ui/SignIn.tsx
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, {SyntheticEvent} from 'react';
 import styles from './sign-in-1-ui-styles/SignIn.module.css'
 
 interface SignInProps {
     login: string
     password: string
     authenticationError: boolean
-    onLoginChanged: ()=> void
-    onPasswordChanged: ()=>void
-    onSubmit: ()=> void
+    errorMessage: string
+    onLoginChanged: (login: string) => void
+    onPasswordChanged: (password: string) => void
+    onSubmit: () => void
 }
 
 const SignIn: React.FC<SignInProps> = ({
-                                           login, password, authenticationError,
+                                           login, password, authenticationError, errorMessage,
                                            onLoginChanged, onPasswordChanged, onSubmit
                                        }) => {
 
     return (
-        <div className={styles.wrapper} >
+        <div className={styles.wrapper}>
             <h2>Форма входа</h2>
             <div className={styles.forms}>
                 <div className={styles.form}>
                     <span>Логин: </span>
                     <input type={"login"} placeholder={'Имя пользователя'}
                            value={login}
-                           onChange={onLoginChanged}/>
+                           onChange={e => onLoginChanged(e.currentTarget.value)}/>
                 </div>
                 <div className={styles.form}>
                     <span>Пароль: </span>
                     <input type={"password"} placeholder={'Пароль'}
                            value={password}
-                           onChange={onPasswordChanged}/>
+                           onChange={e => onPasswordChanged(e.currentTarget.value)}/>
                 </div>
-                {authenticationError && <mark>Имя пользователя или пароль введены не верно.</mark>}
+                {authenticationError && <mark>{errorMessage}</mark>}
                 <button className={styles.button} onClick={onSubmit}>Войти</button>
             </div>
         </div>
--- src/neko-2-sign-in/sign-in-1-ui/SignInContainer.tsx
+++ src/neko-2-sign-in/sign-in-1-ui/SignInContainer.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 import SignIn from "./SignIn";
 
 const SignInContainer: React.FC = () => {
+    const [login, changeLogin] = useState('login');
+    const [password, changePassword] = useState('test password');
+    const [authenticationError, setAuthError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('Имя пользователя или пароль введены не верно.');
     // logic
+    const onLoginChange = (login: string) => {
+        changeLogin(login)
+    };
+    const onPasswordChange = (password: string) => {
+        changePassword(password)
+    };
+    const onSubmitLogin = () => {
+        if (password.length < 7) {
+            setErrorMessage('Длина пароля должна быть больше 7 символов.');
+            setAuthError(true)
+        }
+        console.log(login, password)
+    };
 
     return (
-        <SignIn login={''} password={''} authenticationError={false}
-                onLoginChanged={()=>{}} onPasswordChanged={()=>{}} onSubmit={()=>{}}  />
+        <SignIn login={login} password={password} authenticationError={authenticationError} errorMessage={errorMessage}
+                onLoginChanged={onLoginChange} onPasswordChanged={onPasswordChange} onSubmit={onSubmitLogin}/>
     );
 };
 
