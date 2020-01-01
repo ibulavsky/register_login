5c4b8069df3f5a0453168bbe444cfbf2475f7475
ibulavsky <ibulavsky@gmail.com>
2020-01-01 18:03:04 +0300

    task 8. Added addToCart button.

--- src/neko-9-shop/shop-product-table/ShopProductPage.tsx
+++ src/neko-9-shop/shop-product-table/ShopProductPage.tsx
@@ -7,63 +7,72 @@ import {Button} from "./table/TableElements";
 
 const ShopProductPage: React.FC = () => {
 
-        const productsData = useSelector((store: IAppStore) => store.shop.data.products);
+    const productsData = useSelector((store: IAppStore) => store.shop.data.products);
+
+    const dispatch = useDispatch();
+
+    // callbacks
+    const onAddProduct = (name: string = "Tesla", price: number = 5000) => {
+        dispatch(addProduct(name, price))
+    };
+    const onUpdateProduct = () => {
+        alert('update')
+    };
+    const onDeleteProduct = () => {
+        alert('delete')
+    };
+    const onAddToCart = () => {
+        alert('add to Cart')
+    };
 
 
-        const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getShop());
+    }, []);
 
-        const onAddProduct = (name: string = "Tesla", price: number = 5000) => {
-            dispatch(addProduct(name, price))
-        };
-        const onUpdateProduct = () => {
-            alert('update')
-        };
-        const onDeleteProduct = () => {
-            alert('delete')
-        };
-
-
-        useEffect(() => {
-            dispatch(getShop());
-        }, []);
-
-        const model = [
-            {
-                title: (i: number) => <div key={i} style={{width: '40%'}}> Product </div>,
-                render: (dataItem: any, modelIndex: number, dataIndex: number) => <div key={modelIndex}
-                                                                                       style={{width: '40%'}}> {dataItem.productName}</div>
-            },
-            {
-                title: (i: number) => <div key={i} style={{width: '40%'}}> Price </div>,
-                render:
-                    (dataItem: any, modelIndex: number, dataIndex: number) => <div key={modelIndex}
-                                                                                   style={{width: '40%'}}> {dataItem.price} </div>
-            },
-            {
-                title: (i: number) => <Button key={i} onClickEvent={() => onAddProduct()} name="Add"
-                                              style={{width: '20%'}}/>,
-                render:
-                    (dataItem: any, modelIndex: number, dataIndex: number) => <>
-                        <Button key={modelIndex} name="Update" onClickEvent={() => onUpdateProduct()}
-                                style={{width: '10%'}}/>
-                        <Button key={modelIndex} name="Delete" onClickEvent={() => onDeleteProduct()}
-                                style={{width: '10%'}}/>
-                    </>
-            }
-        ];
+    const model = [
+        {
+            title: (i: number) => <div key={i} style={{width: '35%'}}> Product </div>,
+            render: (dataItem: any, modelIndex: number, dataIndex: number) => <div key={modelIndex}
+                                                                                   style={{width: '35%'}}> {dataItem.productName}</div>
+        },
+        {
+            title: (i: number) => <div key={i} style={{width: '35%'}}> Price </div>,
+            render:
+                (dataItem: any, modelIndex: number, dataIndex: number) => <div key={modelIndex}
+                                                                               style={{width: '35%'}}> {dataItem.price} </div>
+        },
+        {
+            title: (i: number) => <Button key={i} onClickEvent={() => onAddProduct()} name="Add to table"
+                                          style={{width: '20%'}}/>,
+            render:
+                (dataItem: any, modelIndex: number, dataIndex: number) => <>
+                    <Button key={modelIndex} name="Update" onClickEvent={() => onUpdateProduct()}
+                            style={{width: '10%'}}/>
+                    <Button key={modelIndex} name="Delete" onClickEvent={() => onDeleteProduct()}
+                            style={{width: '10%'}}/>
+                </>
+        },
+        {
+            title: (i: number) => <div key={i} style={{width: '10%', textAlign: 'center'}}><a href={'#'}>Cart</a></div>,
+            render:
+                (dataItem: any, modelIndex: number, dataIndex: number) => <>
+                    <Button key={modelIndex} name="Add to Cart" onClickEvent={() => onAddToCart()}
+                            style={{width: '10%'}}/>
+                </>
+        }
+    ];
 
 
-        return (
-            < >
-        <
-        Table
-        model = {model}
-        data = {productsData}
-        />
-    </>
+    return (
+        <>
+            <
+                Table
+                model={model}
+                data={productsData}
+            />
+        </>
     )
-        ;
-    }
-;
+};
 
 export default ShopProductPage;
